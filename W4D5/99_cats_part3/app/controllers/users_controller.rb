f829a2999308371cc3e876130c2e2ac579fa0bc0
Clay Shaw <Claydshaw@gmail.com>
2017-08-25 09:25:41 -0700

    Fix mailers to username

--- W4D5/99_cats_part3/app/controllers/users_controller.rb
+++ W4D5/99_cats_part3/app/controllers/users_controller.rb
@@ -5,13 +5,14 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       login_user!(@user)
+      msg = UserMailer.welcome_email(@user)
+      msg.deliver_now
       redirect_to cats_url
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
     end
-    msg = UserMailer.welcome_email(@user)
-    msg.deliver_now
+
   end
 
   def new
--- W4D5/99_cats_part3/app/mailers/user_mailer.rb
+++ W4D5/99_cats_part3/app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
-  default from: 'from@example.com'
+  default from: '99cats@example.com'
 
   def welcome_email(user)
     @user = user
     @url = 'http://example.com/login'
-    mail(to: user.email, subject: 'Welcome to 99Cats!')
+    mail(to: user.username, subject: 'Welcome to 99Cats!')
   end
-  
+
 end
