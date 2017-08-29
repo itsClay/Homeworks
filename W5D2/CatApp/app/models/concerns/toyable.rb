1c1ec65d47cabf83b63fdce8f1b69cd80a1cf1c5
Clay Shaw <Claydshaw@gmail.com>
2017-08-29 07:40:32 -0700

    update Toyable

--- W5D2/CatApp/app/models/concerns/toyable.rb
+++ W5D2/CatApp/app/models/concerns/toyable.rb
@@ -2,10 +2,11 @@ module Toyable
   extend ActiveSupport::Concern
 
   included do
-
+    has_many :toys, as: :toyable
   end
 
   def receive_toy(name)
-
+    toy = self.toys.find(name: name)
+    toy ? toy : self.toys.create(name: name)
   end
 end
