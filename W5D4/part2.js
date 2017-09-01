26ee0cde08587105ade52c2065fb1af446050d07
Clay Shaw <Claydshaw@gmail.com>
2017-09-01 09:14:48 -0700

    Updates to part2 hws

--- W5D4/part2.js
+++ W5D4/part2.js
@@ -29,3 +29,10 @@ Elephant.prototype.addTrick(trick) = function () {
 Elephant.prototype.play = function () {
   console.log(`${this.name} is ${this.tricks[Math.random(0, this.tricks.length)]}`)
 };
+
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [ellie, charlie, kate, micah];
