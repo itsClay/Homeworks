9f5977244b26c5745915958c1cf1bef15025c0bf
Clay Shaw <Claydshaw@gmail.com>
2017-09-08 09:15:07 -0700

    Initial commit

---
+++ W6D5/Calculator/calculator.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  constructor() {
+    super();
+    this.state = { num1: "", num2: "", result: 0 };
+    this.setNum1 = this.setNum1.bind(this);
+    this.setNum2 = this.setNum2.bind(this);
+  }
+
+  setNum1(event){
+    let num1 = event.target.value;
+    if (!num1) {
+      num1 = "";
+    } else {
+      num1 = parseInt(event.target.value);
+    }
+    this.setState({ num1 });
+  }
+
+  setNum2(event){
+    let num2 = event.target.value;
+    if (!num2) {
+      num2 = "";
+    } else {
+      num2 = parseInt(event.target.value);
+    }
+    this.setState({ num2 });
+  }
+
+  add(e){
+    e.preventDefault();
+    const result = this.state.num1 + this.state.num2;
+    this.setState({ result });
+  }
+
+  subtract(e){
+    e.preventDefault();
+    const result = this.state.num1 - this.state.num2;
+    this.setState({ result });
+  }
+
+  multiply(e){
+    e.preventDefault();
+    const result = this.state.num1 * this.state.num2;
+    this.setState({ result });
+  }
+
+  divide(e){
+    e.preventDefault();
+    const result = this.state.num1 / this.state.num2;
+    this.setState({ result });
+  }
+
+  clear(e){
+    e.preventDefault();
+    this.setState({ num1: "", num2: "", result: 0 });
+  }
+
+
+
+  render(){
+    return (
+      <div>
+        <h1>{this.state.result}</h1>
+        <input>{this.state.setNum1}</input>
+        <input>{this.state.setNum2}</input>
+        <button onClick>{this.clear}</button>
+        <br />
+        <button onClick={this.add} >+</button>
+        <button onClick={this.subtract} >-</button>
+        <button onClick={this.multiply} >*</button>
+        <button onClick={this.divide} >/</button>
+     </div>
+    );
+  }
+
+}
+
+
+export default Calculator;
